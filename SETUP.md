5223814989ad6f8b1459e0121b195fc923e641ba
Justin Makeig <jmpublic+github@makeig.com>
2016-08-01 00:16:57 -0700

    Adds installation for OS X .dmg/.pkg from GA, nightly, and local.

--- SETUP.md
+++ SETUP.md
@@ -1,2 +1,6 @@
 * `virtaulenv env` to create a new virtualenv called “env”
 * `source env/bin/activate` to set virtualenv
+* `pip install -r requirements.txt` to install dependencies
+
+* `git clone git@github.com:google/macops.git`
+* `~/Workspaces/mlvm/env/bin/python ~/Worksapces/macops/gmacpyutil/setup.py install`
--- mlvm.py
+++ mlvm.py
@@ -39,7 +39,11 @@ from docopt import docopt
 import logging
 import os
 import platform
+import subprocess
 import getpass
+#import tempfile
+#import uuid
+import re
 import requests
 from requests.auth import HTTPDigestAuth
 import datetime
@@ -55,8 +59,15 @@ logger.addHandler(log)
 
 def get_release_artifact(major, minor, patch, system=platform.system()):
     if 'Darwin' == system:
-      return 'MarkLogic-' + major + '.' + minor + '-' + patch + '-x86_64.dmg'
+      return 'MarkLogic-' + major + '.' + minor + '-' + patch + '-x86_64'
     raise Exception('Not yet implemented')
+    
+def parse_artifact_from_file(file):
+    pattern = 'MarkLogic-(?:RHEL\d-)?\d{1,2}\.\d-(?:(?:\d{8})|(?:\d{1,2}\.\d{1,2}))[\.\-](?:x86_|amd)64'
+    match = re.findall(pattern, file)
+    if len(match) == 1:
+        return match[0]
+    raise Exception(artifact + ' does not match a MarkLogic version')
 
 def promptCredentials(realm):
     """ Callback to prompt for username and password """
@@ -65,9 +76,9 @@ def promptCredentials(realm):
     return {'user': user, 'password': password}
 
 def get_download_itr(major, minor, patch, is_nightly=False, onAuth=promptCredentials):
-    major = str(int(major)) # 8
-    minor = str(int(minor)) # 0
-    patch = str(patch) # 5.5 or 20160731
+    #major = str(int(major)) # 8
+    #minor = str(int(minor)) # 0
+    #patch = str(patch) # 5.5 or 20160731
         
     if is_nightly:
       # https://root.marklogic.com/nightly/builds/macosx-64/osx-intel64-80-build.marklogic.com/b8_0/pkgs.20160731/MarkLogic-8.0-20160731-x86_64.dmg
@@ -93,8 +104,7 @@ def get_download_itr(major, minor, patch, is_nightly=False, onAuth=promptCredent
       logger.debug(DMC_URL + path)
       return session.get(DMC_URL + path, stream=True).iter_content
 
-
-def download_file(itr, local_filename = 'MarkLogic.dmg', total_size=None, onProgress=None):
+def download_file(itr, local_filename, total_size=None, onProgress=None):
     """ Given a response iterator, write chunks to a file. """
     chunk_size=1024 * 5
     running = 0
@@ -110,8 +120,34 @@ def download_file(itr, local_filename = 'MarkLogic.dmg', total_size=None, onProg
 def show_progress(amt, total, stream=None):
     logger.debug(str(amt) + ' of ' + str(total))
 
-def install_package(package_path):
-    logger.debug(package_path)
+def install_package(path, artifact, alias=None, system=platform.system()):
+    #logger.debug(path)
+    if alias is None:
+        alias = artifact
+    if not os.path.isfile(path):
+        raise Exception(path + ' does not exist')
+    if 'Darwin' == system:
+        #mount_point = tempfile.gettempdir() + '/' + str(uuid.uuid4())
+        #mount_point = ensure_directory(HOME + '/temp') + '/' + str(uuid.uuid4())
+        #logger.debug(mount_point)
+        #subprocess.call(["hdiutil", "attach", path, "-mountpoint", mount_point, "-verbose"]) #, "-nobrowse", "-quiet"])
+        from gmacpyutil import macdisk
+        img = macdisk.Image(path)
+        disks = img.Attach()
+        
+        from gmacpyutil import RunProcess
+        cmd = ['tar', 'xfz', '/Volumes/MarkLogic/' + artifact + '.pkg/Contents/Archive.pax.gz', 
+            '-C', ensure_directory(HOME + '/versions/' + alias)]
+        result = RunProcess(cmd)
+        logger.debug(result)
+        #if 0 != result[2]
+        #    raise Exception(result[1])
+        
+        ensure_directory(HOME + '/versions/' + alias + '/Support/Data')
+        os.chmod(HOME + '/versions/' + alias + '/StartupItems/MarkLogic/MarkLogic', 0775) # TODO: +x, not hard-coded
+    else:
+        raise Exception('Support for ' + system + ' is not yet implemented')
+      
 
 def parse_version(version):
     """ Parses a string, such as `'9.0-20160731'` or `'8.0-5.5'` into a 
@@ -127,7 +163,7 @@ def parse_version(version):
     minor = None
     if len(mm_tokens) > 1:
         minor = int(mm_tokens[1])
-    return {'major': major, 'minor': minor, 'patch': patch}
+    return {'major': str(major), 'minor': str(minor), 'patch': patch}
 
 def ensure_directory(path):
     """ If a directory doesn’t exist at the path create it. """    
@@ -171,8 +207,13 @@ if __name__ == '__main__':
 
     if arguments.get('install'):
         package = None
+        artifact = None
+        alias = arguments.get('<name>')
         if arguments.get('--local'):
             package = arguments.get('<package>')
+            artifact = parse_artifact_from_file(package)
+            if alias is None:
+                alias = '9.0-20160731' # FIXME: Need to parse version out of artifact
         else:
             version = parse_version(arguments.get('<version>'))
             if arguments.get('--today'):
@@ -180,6 +221,8 @@ if __name__ == '__main__':
                   raise Exception('You must not specify a patch release with the --today option')
                 today = datetime.date.today()
                 nightly = today.strftime('%Y%m%d')
+                artifact = get_release_artifact(version.get('major'), version.get('minor'), nightly)
+                dest = ensure_directory(HOME + '/downloads') + '/' + artifact + '.dmg'
                 package = download_file(
                     get_download_itr(
                         version.get('major'), 
@@ -187,19 +230,20 @@ if __name__ == '__main__':
                         nightly, 
                         is_nightly=True
                     ), 
-                    local_filename = ensure_directory(HOME + '/downloads') + '/MarkLogic.dmg', 
+                    local_filename = dest, 
                     onProgress=show_progress
                 )
             else:
-                logger.debug(version)
+                artifact = get_release_artifact(version.get('major'), version.get('minor'), version.get('patch'))
+                dest = ensure_directory(HOME + '/downloads') + '/' + artifact + '.dmg'
                 package = download_file(
                     get_download_itr(
                         version.get('major'),
                         version.get('minor'),
                         version.get('patch')
                     ),
-                    local_filename = ensure_directory(HOME + '/downloads') + '/MarkLogic.dmg',
+                    local_filename = dest,
                     onProgress=show_progress
                 )
 
-        install_package(package)            
+        install_package(package, artifact, alias=alias)            
