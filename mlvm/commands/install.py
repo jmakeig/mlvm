32afa875217dd38d045b0dceef77255bfc386bac
Justin Makeig <jmpublic+github@makeig.com>
2016-08-07 21:57:26 -0700

    Refactors install into its own command module
    Probably still bugs.

--- mlvm/commands/install.py
+++ mlvm/commands/install.py
@@ -14,3 +14,146 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+import logging
+import datetime
+
+from mlvm.settings import HOME, SYSTEM, USER
+import mlvm.filesystem as fs
+import mlvm.cli as cli
+import mlvm.versions as versions
+
+logger = logging.getLogger('mlvm')
+
+""" `True` to bypass the actual download for testing. 
+Assumes you’ve already got the artifact downloaded to the downloads folder. """
+DUMMY = False # Make sure not to check this in as True
+
+def get_download_itr(major, minor, patch, is_nightly = False, onAuth = cli.promptCredentials):
+    import requests
+    from requests.auth import HTTPDigestAuth
+
+    if DUMMY:
+        return 'DUMMY'
+
+    if is_nightly:
+        # https://root.marklogic.com/nightly/builds/macosx-64/osx-intel64-80-build.marklogic.com/b8_0/pkgs.20160731/MarkLogic-8.0-20160731-x86_64.dmg
+        ROOT_URL = 'https://root.marklogic.com'
+        # TODO: Platform-specific
+        auth = onAuth(ROOT_URL)
+        # FIXME: Cross-platform
+        url = ROOT_URL+ '/nightly/builds/macosx-64/osx-intel64-' + major + minor + '-build.marklogic.com/HEAD/pkgs.' + patch + '/' + versions.get_release_artifact(major, minor, patch) + '.dmg'
+        logger.debug(url)
+        return requests.get(url, auth = HTTPDigestAuth(auth.get('user'), auth.get('password')), stream = True).iter_content
+    else:
+        DMC_URL = 'https://developer.marklogic.com'
+        auth = onAuth(DMC_URL)
+        session = requests.Session()
+        response = session.post(DMC_URL + '/login', data={'email': auth.get('user'), 'password': auth.get('password')})
+        # TODO: Handle non-200 response
+        # > 200 OK
+        # > {"status":"ok","name":"Justin Makeig"}
+        response = session.post(DMC_URL + '/get-download-url', data={'download': '/download/binaries/' + major + '.' + minor + '/' + get_release_artifact(major, minor, patch) + '.dmg'}) # FIXME: Platform-specific
+        # TODO: Handle non-200 response
+        # > 200 OK
+        # > {"status":"ok","path":"/download/binaries/8.0/MarkLogic-8.0-5.5-x86_64.dmg?t=*************/&email=whoami%40example.com"}
+        logger.debug(response.status_code)
+        if 200 != response.status_code:
+            raise Exception(response.text) # TODO: Better exceptions?
+        path = response.json().get('path')
+        logger.debug(DMC_URL + path)
+        return session.get(DMC_URL + path, stream=True).iter_content
+
+def download_file(itr, local_filename, total_size = None, onProgress = None):
+    """ Given a response iterator, write chunks to a file. """
+
+    if DUMMY:
+        return local_filename
+    
+    chunk_size = 1024 * 5
+    running = 0
+    with open(local_filename, 'wb') as f:
+        for chunk in itr(chunk_size=chunk_size): 
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                running += chunk_size
+                if onProgress is not None:
+                    onProgress(running, total_size)
+    return local_filename
+
+def install_package(path, artifact, alias=None, system = SYSTEM):
+    if alias is None:
+        alias = artifact
+    if not os.path.isfile(path):
+        raise Exception(path + ' does not exist')
+    
+    logger.info('Installing %s from %s with artifact %s', alias, path, artifact)
+    
+    if 'Darwin' == system:
+        try:
+            from gmacpyutil import macdisk
+            img = macdisk.Image(path)
+            disk = img.Attach()
+        
+            from gmacpyutil import RunProcess
+            # pax.gz doesn’t seem to be properly supported by Python’s tar library
+            cmd = ['tar', 'xfz', '/Volumes/MarkLogic/' + artifact + '.pkg/Contents/Archive.pax.gz', 
+                '-C', fs.ensure_directory(HOME + '/versions/' + alias)] 
+            result = RunProcess(cmd)
+            logger.debug(result)
+            if 0 != result[2]:
+                raise Exception(result[1])
+            fs.ensure_directory(HOME + '/versions/' + alias + '/Support/Data')
+            os.chmod(HOME + '/versions/' + alias + '/StartupItems/MarkLogic/MarkLogic', 0755) # TODO: +x, not hard-coded
+        finally:
+            logger.debug('Detaching disk image')
+            img.Detach(force=True)
+    else:
+        raise Exception('Support for ' + system + ' is not yet implemented')
+
+def install(arguments):
+    fs.ensure_directory(HOME)
+
+    if arguments.get('install'):
+        package = arguments.get('<package>')
+        artifact = None
+        alias = arguments.get('<name>')
+        if arguments.get('--local'):
+            artifact = versions.parse_artifact_from_file(package)
+            if alias is None:
+                alias = versions.serialize_version(versions.parse_version(artifact))
+        else:
+            version = versions.parse_version(arguments.get('<version>'))
+            if arguments.get('--today'):
+                if version.get('patch') is not None:
+                  raise Exception('You must not specify a patch release with the --today option')
+                today = datetime.date.today()
+                nightly = today.strftime('%Y%m%d')
+                artifact = versions.get_release_artifact(version.get('major'), version.get('minor'), nightly)
+                alias = versions.serialize_version(versions.parse_version(artifact))
+                # FIXME: Cross-platform
+                dest = fs.ensure_directory(HOME + '/downloads') + '/' + artifact + '.dmg'
+                package = download_file(
+                    get_download_itr(
+                        version.get('major'), 
+                        version.get('minor'), 
+                        nightly, 
+                        is_nightly=True
+                    ), 
+                    local_filename = dest, 
+                    onProgress=cli.show_progress
+                )
+            else:
+                artifact = versions.get_release_artifact(version.get('major'), version.get('minor'), version.get('patch'))
+                # FIXME: Cross-platform
+                dest = fs.ensure_directory(HOME + '/downloads') + '/' + artifact + '.dmg'
+                package = download_file(
+                    get_download_itr(
+                        version.get('major'),
+                        version.get('minor'),
+                        version.get('patch')
+                    ),
+                    local_filename = dest,
+                    onProgress=show_progress
+                )
+        install_package(package, artifact, alias=alias)   
--- mlvm/main.py
+++ mlvm/main.py
@@ -15,16 +15,6 @@
 # limitations under the License.
 
 import logging
-import sys
-import os
-import platform
-import subprocess
-import getpass
-import re
-import requests
-from requests.auth import HTTPDigestAuth
-import datetime
-
 
 logger = logging.getLogger('mlvm')
 logger.setLevel(logging.DEBUG)
@@ -34,158 +24,12 @@ log.setLevel(logging.DEBUG)
 log.setFormatter(logging.Formatter('%(levelname)s - %(message)s'))
 logger.addHandler(log)
 
-################################################################################
-from versions import get_release_artifact, parse_artifact_from_file, parse_version, serialize_version
-################################################################################
-
-def promptCredentials(realm):
-    """ Callback to prompt for username and password """
-    user = raw_input('Username for ' + realm + ': ') # TODO: Validation
-    password = getpass.getpass('Password for ' + user + ' on ' + realm + ': ')
-    return {'user': user, 'password': password}
-
-""" `True` to bypass the actual download for testing. 
-Assumes you’ve already got the artifact downloaded to the downloads folder. """
-DUMMY = True # Make sure not to check this in as True
-
-def get_download_itr(major, minor, patch, is_nightly=False, onAuth=promptCredentials):
-    
-    if DUMMY:
-        return 'DUMMY'
-
-    if is_nightly:
-        # https://root.marklogic.com/nightly/builds/macosx-64/osx-intel64-80-build.marklogic.com/b8_0/pkgs.20160731/MarkLogic-8.0-20160731-x86_64.dmg
-        ROOT_URL = 'https://root.marklogic.com'
-        # TODO: Platform-specific
-        auth = onAuth(ROOT_URL)
-        # FIXME: Cross-platform
-        url = ROOT_URL+ '/nightly/builds/macosx-64/osx-intel64-' + major + minor + '-build.marklogic.com/HEAD/pkgs.' + patch + '/' + get_release_artifact(major, minor, patch) + '.dmg'
-        logger.debug(url)
-        return requests.get(url, auth=HTTPDigestAuth(auth.get('user'), auth.get('password')), stream=True).iter_content
-    else:
-        DMC_URL = 'https://developer.marklogic.com'
-        auth = onAuth(DMC_URL)
-        session = requests.Session()
-        response = session.post(DMC_URL + '/login', data={'email': auth.get('user'), 'password': auth.get('password')})
-        # TODO: Handle non-200 response
-        # > 200 OK
-        # > {"status":"ok","name":"Justin Makeig"}
-        response = session.post(DMC_URL + '/get-download-url', data={'download': '/download/binaries/' + major + '.' + minor + '/' + get_release_artifact(major, minor, patch) + '.dmg'}) # FIXME: Platform-specific
-        # TODO: Handle non-200 response
-        # > 200 OK
-        # > {"status":"ok","path":"/download/binaries/8.0/MarkLogic-8.0-5.5-x86_64.dmg?t=*************/&email=whoami%40example.com"}
-        logger.debug(response.status_code)
-        if 200 != response.status_code:
-            raise Exception(response.text) # TODO: Better exceptions?
-        path = response.json().get('path')
-        logger.debug(DMC_URL + path)
-        return session.get(DMC_URL + path, stream=True).iter_content
-
-def download_file(itr, local_filename, total_size=None, onProgress=None):
-    """ Given a response iterator, write chunks to a file. """
-
-    if DUMMY:
-        return local_filename
-    
-    chunk_size = 1024 * 5
-    running = 0
-    with open(local_filename, 'wb') as f:
-        for chunk in itr(chunk_size=chunk_size): 
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
-                running += chunk_size
-                if onProgress is not None:
-                    onProgress(running, total_size)
-    return local_filename
-
-def show_progress(amt, total, stream=None):
-    logger.debug(str(amt) + ' of ' + str(total))
-
-def install_package(path, artifact, alias=None, system=platform.system()):
-    if alias is None:
-        alias = artifact
-    if not os.path.isfile(path):
-        raise Exception(path + ' does not exist')
-    
-    logger.info('Installing %s from %s with artifact %s', alias, path, artifact)
-    
-    if 'Darwin' == system:
-        try:
-            from gmacpyutil import macdisk
-            img = macdisk.Image(path)
-            disk = img.Attach()
-        
-            from gmacpyutil import RunProcess
-            # pax.gz doesn’t seem to be properly supported by Python’s tar library
-            cmd = ['tar', 'xfz', '/Volumes/MarkLogic/' + artifact + '.pkg/Contents/Archive.pax.gz', 
-                '-C', ensure_directory(HOME + '/versions/' + alias)] 
-            result = RunProcess(cmd)
-            logger.debug(result)
-            if 0 != result[2]:
-                raise Exception(result[1])
-            ensure_directory(HOME + '/versions/' + alias + '/Support/Data')
-            os.chmod(HOME + '/versions/' + alias + '/StartupItems/MarkLogic/MarkLogic', 0755) # TODO: +x, not hard-coded
-        finally:
-            logger.debug('Detaching disk image')
-            img.Detach(force=True)
-    else:
-        raise Exception('Support for ' + system + ' is not yet implemented')
-      
-def ensure_directory(path):
-    """ If a directory doesn’t exist at the path create it. """    
-    if not os.path.isdir(path):
-      logger.debug('Creating ' + path + ' becuase it does not yet exist')
-      os.makedirs(path)
-    return path
-
 from settings import HOME, SYSTEM
 
-def route_command(arguments):     
-    ensure_directory(HOME)
-
+def route_command(arguments):
     if arguments.get('install'):
-        package = arguments.get('<package>')
-        artifact = None
-        alias = arguments.get('<name>')
-        if arguments.get('--local'):
-            artifact = parse_artifact_from_file(package)
-            if alias is None:
-                alias = serialize_version(parse_version(artifact))
-        else:
-            version = parse_version(arguments.get('<version>'))
-            if arguments.get('--today'):
-                if version.get('patch') is not None:
-                  raise Exception('You must not specify a patch release with the --today option')
-                today = datetime.date.today()
-                nightly = today.strftime('%Y%m%d')
-                artifact = get_release_artifact(version.get('major'), version.get('minor'), nightly)
-                alias = serialize_version(parse_version(artifact))
-                # FIXME: Cross-platform
-                dest = ensure_directory(HOME + '/downloads') + '/' + artifact + '.dmg'
-                package = download_file(
-                    get_download_itr(
-                        version.get('major'), 
-                        version.get('minor'), 
-                        nightly, 
-                        is_nightly=True
-                    ), 
-                    local_filename = dest, 
-                    onProgress=show_progress
-                )
-            else:
-                artifact = get_release_artifact(version.get('major'), version.get('minor'), version.get('patch'))
-                # FIXME: Cross-platform
-                dest = ensure_directory(HOME + '/downloads') + '/' + artifact + '.dmg'
-                package = download_file(
-                    get_download_itr(
-                        version.get('major'),
-                        version.get('minor'),
-                        version.get('patch')
-                    ),
-                    local_filename = dest,
-                    onProgress=show_progress
-                )
-        install_package(package, artifact, alias=alias)            
+        from mlvm.commands.install import install  
+        install(arguments) # TODO: Pass decouple arguments         
     elif arguments.get('prepare'):
         from mlvm.commands.prepare import prepare
         prepare()
