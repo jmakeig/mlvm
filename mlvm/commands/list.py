e14fbf1fbf975400433d694160eecc14983cb3c9
Justin Makeig <jmpublic+github@makeig.com>
2016-08-11 00:46:34 -0700

    Implements list command

--- mlvm/commands/list.py
+++ mlvm/commands/list.py
@@ -22,11 +22,60 @@ from mlvm.settings import HOME, SYSTEM
 
 logger = logging.getLogger('mlvm')
 
+import mlvm.versions
+
 def list(writer):
-   for name in os.listdir('/Users/jmakeig/.mlvm/versions'):
-       writer.write(name + '\n') 
+    versions_dir = HOME + '/versions'
+    # for name in sorted(os.listdir(versions_dir)):
+    #     if '.current' != name:
+    #         writer.write('{version}'.format(**get_version_from_path(versions_dir + '/' + name)) + '\n')
+    
+    versions = [
+            get_version_from_path(versions_dir + '/' + dir) 
+        for dir in os.listdir(versions_dir) 
+        if '.current' != dir
+    ]
+    
+    print '\n'.join([
+            display_version(ver) 
+        for ver in sorted(versions, lambda x, y: cmp(
+            x['alias'] if x['alias'] is not None else x['version'],
+            y['alias'] if y['alias'] is not None else y['version']
+        ))
+    ])
+
+def display_version(version):
+    str = ''
+    if version['is_current']:
+        str = str + '* '
+    else: 
+        str = str + '  '
+
+    if version['alias'] is not None:
+        str = str + version['alias'] + ' -> '
+    
+    return str + version['version']
 
 def get_versions(path):
-    return ''
+    print(os.listdir(path))
+
+def get_current_version():
+    return 'asdf'
+
+def get_version_from_path(path):
+    version = os.path.basename(path)
+    alias = None
+    if os.path.islink(path):
+        version = os.path.basename(os.path.realpath(path))
+        alias = os.path.basename(path)
+    
+    return {
+        'version': version,
+        'alias': alias,
+        'is_current': get_current_version() == version,
+        'is_local': True
+    }
+   
+
 
 # TODO: Figure out which one is current
